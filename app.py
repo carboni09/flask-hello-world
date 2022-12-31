ade6c552661e3d4d6fe0cf24597fd67fd5f9e7a4
carboni09 <caleb.adegbite@hotmail.com>
2022-12-31 01:13:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,40 @@
 from flask import Flask
 app = Flask(__name__)
 
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.interval import IntervalTrigger
+
+
+scheduler = BackgroundScheduler()
+scheduler.start()
+
+
+def job_function():
+    print("Hello, World!")
+
+
+scheduler.add_job(
+    func=job_function,
+    trigger=IntervalTrigger(seconds=15),
+    id='my_job_id',
+    name='Print "Hello, World!" every minute',
+    replace_existing=True)
+
+
+
+
+@app.before_first_request
+def initialize():
+    # ... other initialization code ...
+    print('init')
+    #scheduler.start()
+
+@app.teardown_appcontext
+def shutdown_scheduler(exception=None):
+    scheduler.shutdown()
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, World!!!!!!'
+    
+app.run(host='0.0.0.0', port=81)
