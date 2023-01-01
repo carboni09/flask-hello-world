d4229d1d4aa3c5573d15f6e751d07bedf1d40d36
carboni09 <caleb.adegbite@hotmail.com>
2023-01-01 20:03:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,37 @@
 from flask import Flask
 app = Flask(__name__)
 
-from apscheduler.schedulers.background import BackgroundScheduler
-from apscheduler.triggers.interval import IntervalTrigger
+#from apscheduler.schedulers.background import BackgroundScheduler
+#from apscheduler.triggers.interval import IntervalTrigger
 
 
-scheduler = BackgroundScheduler()
-scheduler.start()
+#scheduler = BackgroundScheduler()
+#scheduler.start()
 
 
 def job_function():
     print("Hello, World!")
 
 
-scheduler.add_job(
-    func=job_function,
-    trigger=IntervalTrigger(seconds=15),
-    id='my_job_id',
-    name='Print "Hello, World!" every minute',
-    replace_existing=True)
+# scheduler.add_job(
+#     func=job_function,
+#     trigger=IntervalTrigger(seconds=15),
+#     id='my_job_id',
+#     name='Print "Hello, World!" every minute',
+#     replace_existing=True)
 
 
 
 
-@app.before_first_request
-def initialize():
-    # ... other initialization code ...
-    print('init')
-    #scheduler.start()
+# @app.before_first_request
+# def initialize():
+#     # ... other initialization code ...
+#     print('init')
+#     #scheduler.start()
 
-@app.teardown_appcontext
-def shutdown_scheduler(exception=None):
-    scheduler.shutdown()
+# @app.teardown_appcontext
+# def shutdown_scheduler(exception=None):
+#     scheduler.shutdown()
 
 @app.route('/')
 def hello_world():
