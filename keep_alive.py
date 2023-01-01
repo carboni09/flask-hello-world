52ac4267bc86c34f47f495a144a7317e3f18e143
carboni09 <caleb.adegbite@hotmail.com>
2023-01-01 21:05:40 +0100

    adds discord keep alive

--- keep_alive.py
+++ keep_alive.py
@@ -5,10 +5,10 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+    return "Hello. I am alive and well!"
 
 def run():
-  app.run(host='0.0.0.0',port=8080)
+  app.run(host='0.0.0.0')
 
 def keep_alive():
     t = Thread(target=run)
